5dfcfe836dce879ac57bc27062931fef7f175489
delbert <54856174+delbert@users.noreply.github.com>
2019-09-30 14:36:36 -0400

    updating parameter default handling

--- launch.py
+++ launch.py
@@ -15,9 +15,9 @@ if os.environ[ "AZ_BATCH_IS_CURRENT_NODE_MASTER" ] == "true":
     print ( "sleeping" )
     time.sleep ( 60 )
     print ( "waking up and running script" )
-    os.system( "python ray_cluster_addrs.py --redis_address {} --redis_password='password'".format( os.environ[ "AZ_BATCH_MASTER_NODE" ] ,
+    os.system( "python ray_cluster_addrs.py --redis_address {} --redis_password='{}'".format( os.environ[ "AZ_BATCH_MASTER_NODE" ] ,
                                                                                                         args.redis_password ) )
 else:
     print( "worker node" )
-    os.system( "ray start --redis-address={} --redis-password='password'".format( os.environ[ "AZ_BATCH_MASTER_NODE" ] ,
+    os.system( "ray start --redis-address={} --redis-password='{}'".format( os.environ[ "AZ_BATCH_MASTER_NODE" ] ,
                                                                                                         args.redis_password ) )
--- ray_cluster_addrs.py
+++ ray_cluster_addrs.py
@@ -5,8 +5,8 @@ import argparse
 import ray
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--redis_address", default="localhost", type=str)
-parser.add_argument("--redis_password", default="password", type=str)
+parser.add_argument( "--redis_address", default="localhost", type=str )
+parser.add_argument( "--redis_password", default="password", type=str )
 args = parser.parse_args()
 
 ray.init( redis_address = args.redis_address , redis_password = args.redis_password ) 
